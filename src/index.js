e187f1a94cab160b103e09aae08a8f901db015e2
Mehmet Aydar <ceza_mehmet8@hotmail.com>
2022-09-02 01:51:35 +0300

    this file updated

--- src/index.js
+++ src/index.js
@@ -1,46 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const products = [
-    {title: 'Cabbage', isFruit: false, id: 1},
-    {title: 'Garlic', isFruit: false, id: 2},
-    {title: 'Apple', isFruit: true, id: 3}
-];
 
-const ShoppingList = () => {
-
-    const listItems = products.map((product) => {
-        return (
-            <li
-                key={product.id}
-                style={{
-                    color: product.isFruit ? 'magenta' : 'darkgreen'
-                }}
-            >
-                {product.title}
-            </li>
-        )
-    });
-
-    return (
-        <ul>
-            {listItems}
-        </ul>
-    )
-
-}
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    <ShoppingList />
+    
 )
 
 
 
 
 /*
-    PRACTICE 1
+    PRACTICE-1
 
     // Creating and nesting components --> Componentleri Oluşturma ve İç İçe Yerleştirme
 
@@ -71,7 +44,7 @@ root.render(
 
 
 /*
-    PRACTICE 2
+    PRACTICE-2
 
     // Writing markup with JSX --> JSX İle Yazma   &&   Adding styles --> Stil Ekleme
 
@@ -112,7 +85,7 @@ root.render(
 
 
 /*
-    PRACTICE 3
+    PRACTICE-3
 
     // Rendering lists --> Liste Oluşturma
 
@@ -155,3 +128,154 @@ root.render(
     )
 */
 
+
+
+/*
+    PRACTICE-4
+
+    // Responding to events --> Olaylara Yanıt Vermek
+
+    import React from 'react';
+    import ReactDOM from 'react-dom/client';
+
+
+    const MyButton = () => {
+
+        const handleClick = () => {
+            alert("You clicked me")
+        }
+
+        return (
+            <button onClick={handleClick}>Click Me</button>
+        )
+
+    }
+
+
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+    root.render(
+        <MyButton />
+    )
+*/
+
+
+
+/*
+    PRACTICE-5 --> Using Hooks
+
+    // useState Hook --> Çoğu zaman, Componentimizin bazı bilgileri "hatırlamasını" ve "görüntülemesini" isteriz. Örneğin, bir düğmenin kaç kez tıklandığını saymak isteyebiliriz. Bunu yapmak için componentimize durum(state) eklemeliyiz.
+
+
+    import React, { useState } from 'react';
+    import ReactDOM from 'react-dom/client';
+
+
+    const MyButton = () => {
+        const [count, setCount] = useState(0);
+        const [count1, setCount1] = useState(0);
+
+        const Increase = () => {
+            setCount(count + 1);
+        };
+
+        const Decrease = () => {
+            setCount1(count1 - 1);
+        };
+
+        return (
+            <div>
+                <button onClick={Increase}> Increase count:</button>
+                <p>{count}</p>
+                <button onClick={Decrease}> Decrease count1:</button>
+                <p>{count1}</p>
+            </div>
+        )
+    };
+
+
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+    root.render(
+        <MyButton />
+    )
+*/
+
+
+
+/*
+    PRACTICE-6
+
+    import React, { useState } from 'react';
+    import ReactDOM from 'react-dom/client';
+
+
+    const MyButton = () => {
+        const [count, setCount] = useState(0);
+
+        const handleClick = () => {
+            setCount(count + 1)
+        }
+
+        return (
+            <button onClick={handleClick}>
+                Clicked {count} times
+            </button>
+        );
+    };
+
+    const MyApp = () => {
+        return (
+            <div>
+                <h1>Counters that update separately</h1>
+                <MyButton />
+                <MyButton />
+            </div>
+        );
+    };
+
+
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+    root.render(
+        <MyApp />
+    )
+*/
+
+
+
+/*
+    PRACTICE-7
+
+    import React, { useState } from 'react';
+    import ReactDOM from 'react-dom/client';
+
+
+    const MyButton = ({ count, onClick }) => {
+        return (
+            <button onClick={onClick}>
+                Clicked {count} times
+            </button>
+        );
+    };
+
+    const MyApp = () => {
+        const [count, setCount] = useState(0);
+
+        const handleClick = () => {
+            setCount(count + 1);
+        }
+
+        return (
+            <div>
+                <h1>Counters that update together</h1>
+                <MyButton count={count} onClick={handleClick}/>
+                <MyButton count={count}  onClick={handleClick}/>
+            </div>
+        )
+
+    };
+
+
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+    root.render(
+        <MyApp />
+    )
+*/
