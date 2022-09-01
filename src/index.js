82e818d771f6b067765fbaa578719690b8084768
Mehmet Aydar <ceza_mehmet8@hotmail.com>
2022-09-02 01:52:58 +0300

    this file updated

--- src/index.js
+++ src/index.js
@@ -2,11 +2,35 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
 
+const MyButton = ({ count, onClick }) => {
+    return (
+        <button onClick={onClick}>
+            Clicked {count} times
+        </button>
+    );
+};
+
+const MyApp = () => {
+    const [count, setCount] = useState(0);
+
+    const handleClick = () => {
+        setCount(count + 1);
+    }
+
+    return (
+        <div>
+            <h1>Counters that update together</h1>
+            <MyButton count={count} onClick={handleClick}/>
+            <MyButton count={count}  onClick={handleClick}/>
+        </div>
+    )
+
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-    
+    <MyApp />
 )
 
 
